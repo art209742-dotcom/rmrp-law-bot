fd71f45dcc28adb1e617644c4f039e948c3bf433
art209742-dotcom <art209742@gmail.com>
2026-08-16 01:54:33 +0300

    Fix port assignment in proxy.py

--- proxy.py
+++ proxy.py
@@ -129,5 +129,4 @@ def get_codexes():
     return jsonify(list(ALL_LAWS.keys()))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
